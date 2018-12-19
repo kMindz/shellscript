95951373b4530d3a203dc5762396eb0cd9aff24d
Privet-mir <37708186+Privet-mir@users.noreply.github.com>
2018-12-19 10:16:37 +0530

    Create explorer.sh

---
+++ explorer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+    sudo apt-get update
+    sudo apt-get install postgresql postgresql-contrib
+    sudo apt-get install jq
+    git clone https://github.com/kMindz/blockchain-explorer.git
+    cd blockchain-explorer/app/persistence/fabric/postgreSQL/db
+    ./createdb.sh
+    cd
+     cd blockchain-explorer
+     npm install
+     cd client
+     npm install
+     npm run build
+
+echo "enter config type 1) fabric-de-server 2) fabric-tools"
+echo "enter option as 1 or 2"
+read options
+
+if [ $options == 1 ]; then
+cd /home/qq/blockchain-explorer/app/platform/fabric
+cp fabric-dev-servers.json  config.json
+echo "fabric-dev-servers copied to config.json"
+else
+cd /home/qq/blockchain-explorer/app/platform/fabric
+ cp  fabric-tools.json  config.json
+echo "fabric-tools copied to config.json"
+
+fi
